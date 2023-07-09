da6f2d49b11e47d73285d55e4acc67f234fc1025
sandeep-guttula <sandeepguttula2002@gmail.com>
2023-07-09 11:36:01 +0530

    initila commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Home from "./screens/home/Home";
+import { Routes, Route } from "react-router-dom";
+import Login from "./screens/login/Login";
+import PrivateRoute from "./routes/PrivateRoute";
+import SignUp from "./screens/signup/SignUp";
+import UserDetails from "./screens/signup/UserDetails";
+import VerifyEmail from "./screens/signup/VerifyEmail";
+import Profile from "./screens/profile/Profile";
+import AboutUs from "./screens/aboutUs/AboutUs";
+import Help from "./screens/help/Help";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route element={<PrivateRoute />}>
+        <Route path="/" element={<Home />} exact />
+      </Route>
+      <Route path="/" element={<Home />} />
+      <Route path="/signup" element={<SignUp />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/verifyaccount" element={<VerifyEmail />} />
+      <Route path="/signup/user-details" element={<UserDetails />} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/aboutus" element={<AboutUs />} />
+      <Route path="/Help" element={<Help />} />
+    </Routes>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { StateProvider } from "./utils/StateProvider";
+import reducer, { initialState } from "./utils/reducer";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./screens/home/Home";
+import Login from "./screens/login/Login";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <StateProvider initialState={initialState} reducer={reducer}>
+      <Router>
+        <App />
+      </Router>
+    </StateProvider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
